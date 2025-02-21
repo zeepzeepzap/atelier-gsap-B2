7622c156623c33eb96cb44105ac6f01f519fe48c
Clara Dubuc <meisakapro@gmail.com>
2025-02-21 01:57:23 +0100

    Adding animation of a text from Section 1.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 // GSAP Modules
 gsap.registerPlugin(ScrollTrigger, Observer, ScrollToPlugin, Draggable, MotionPathPlugin);
+
+gsap.to(".intro", {
+    x: 80,
+    y: -10,
+    rotation: 2,
+    duration: 4,
+    ease: "power1.inOut",
+    yoyo: true,
+    repeat: -1
+});
